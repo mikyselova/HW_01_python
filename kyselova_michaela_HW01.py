2d02eb5b25dc64d4c377a6f9ca4234a27eb8ac50
mikyselova <mikyselova@seznam.cz>
2025-04-24 23:02:08 +0200

    Commitnutí úkolu

---
+++ kyselova_michaela_HW01.py
@@ -0,0 +1,20 @@
+import json
+
+with open("alice.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+text = text.lower()
+text = text.replace(" ", "")
+text = text.replace("\n", "")
+
+cetnost_znaku = {}
+for znak in text:
+    if znak not in cetnost_znaku:
+        cetnost_znaku[znak] = 1
+    else:
+        cetnost_znaku[znak] += 1
+
+cetnost_znaku = dict(sorted(cetnost_znaku.items()))
+
+with open('hw01_output.json', 'w', encoding='utf-8') as file:
+    json.dump(cetnost_znaku, file, indent=4, ensure_ascii=False)
